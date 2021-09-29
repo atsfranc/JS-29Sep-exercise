83942d42851a02bd69a0c31ac29d8c4e040a23a6
Francis Ang <francisang@Francis-MacBook-Pro.local>
2021-09-29 15:29:24 +0800

    exercise-2

---
+++ exercise-2.js
@@ -0,0 +1,10 @@
+
+function star(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr[i].length; j++) {
+			console.log(arr[i][j]);
+		}
+	}
+	
+}
+star([["*"], ["**"], ["***"], ["****"], ["*****"]]);
